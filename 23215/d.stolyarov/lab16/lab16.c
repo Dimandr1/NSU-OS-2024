b0a2c9749c40599dcb66b4a6482e6591a739a32e
Dimandr <Dmitry221556@yandex.ru>
2024-11-21 18:55:41 +0700

    lab16

--- 23215/d.stolyarov/lab16/lab16.c
+++ 23215/d.stolyarov/lab16/lab16.c
@@ -4,20 +4,20 @@
 #include <termios.h>
 #include <fcntl.h>
 int main(){
-    printf("asd? [y/n]:\n");
+    printf("asd? [y/n]: ");
     int fd = open("/dev/tty", O_RDONLY);
     char c;
     struct termios tty, save;
     tcgetattr(fd, &tty);
     save = tty;
-    tty.c_lflag &= ~(ICANON | ECHO);
+    tty.c_lflag &= ~(ICANON);
     tcsetattr(fd, TCSAFLUSH, &tty);
     read(fd, &c, 1);
     if(c == 'y' || c == 'n'){
-        printf("ended succeessfully\n");
+        printf("\nended succeessfully\n");
     }
     else{
-        printf("wrong symbol\n");
+        printf("\nwrong symbol\n");
     }
     tcsetattr(fd, TCSANOW, &save);
     exit(0);
